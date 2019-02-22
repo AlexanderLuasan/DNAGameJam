30a4ea81ff6f71309136fc45ac38f5b5185d55ea
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-02-22 10:01:22 +0800

    added first two main classes

---
+++ DNAGameJam/CollisionObj.py
@@ -0,0 +1,18 @@
+
+
+
+class CollisionObj():
+    def __init__ (self,collisionRect,DrawingRect):
+        self.collisionRect = collisionRect
+        self.DrawingRect = DrawingRect
+
+    def Update(self):
+        pass
+    def getCollision(self):
+        return self.collisionRect
+    def getDrawing(self):
+        return self.DrawingRect
+    def collide(self,otherObj):
+        pass
+    def preDraw():
+        pass
---
+++ DNAGameJam/screen.py
@@ -0,0 +1,8 @@
+
+
+
+class window():
+    def __init__(self,screenSurface):
+        self.screen = screenSurface;
+        self.camx = 0
+        self.camy = 0
