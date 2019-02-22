14636fb431524363a64c9319f9342eec7013b88f
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-02-22 10:12:32 +0800

    working startingpoints

--- DNAGameJam/DNAGameJam.py
+++ DNAGameJam/DNAGameJam.py
@@ -1,5 +1,5 @@
 import pygame
-
+import screen
 pygame.init()
 
 #jonathan's comment
@@ -8,7 +8,7 @@ timmer = pygame.time.Clock()
 swidth = 600
 sheight = 400
 pygame.display.set_mode([swidth,sheight])
-screen = pygame.display.get_surface();
+gamescreen=screen.window(pygame.display.get_surface())
 
 shape = pygame.Rect(0,0,40,40)
 shape.center = (swidth/2,sheight/2)
@@ -36,10 +36,10 @@ while(not done):
     if(shape.right>swidth or shape.left<0):
         xvel= xvel * -1;
 
-    screen.fill(pygame.Color(200,200,200,255))
-    screen.blit(starimg,shape)
+    #screen.
+    #screen.blit(starimg,shape)
     
-    pygame.draw.rect(screen,pygame.Color(0, 0, 0, 255),shape,1)
+    gamescreen.drawRect(shape)
     pygame.display.flip()
 
     timmer.tick(60)
--- DNAGameJam/screen.py
+++ DNAGameJam/screen.py
@@ -1,8 +1,13 @@
 
-
-
+import pygame.draw as d
+from pygame import Color
 class window():
     def __init__(self,screenSurface):
         self.screen = screenSurface;
         self.camx = 0
         self.camy = 0
+    def drawRect(self,rect):
+        self.screen.fill(Color(200,200,200,255))
+        d.rect(self.screen,Color(0, 0, 0, 255),rect,0)
+    def flip(self):
+        pass
