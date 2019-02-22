e7de6329223b36d407dfd59ff24d806ccd03ed04
Sungho PARK <andrepark3-c@ad.cityu.edu.hk>
2019-02-22 13:15:17 +0800

    Have editted levelload and allowed import xml files

--- DNAGameJam/levelload.py
+++ DNAGameJam/levelload.py
@@ -1,4 +1,7 @@
 from xml.dom import minidom
+import pygame
+from plat import plat
+
 def go():
     mydoc = minidom.parse('test.tmx')
 
@@ -7,3 +10,17 @@ def go():
     print("test: ")
     print(platform[1].attributes['id'].value);
     print("test ended")
+
+def load():
+    mydoc = minidom.parse('test.tmx')
+    platform = mydoc.getElementsByTagName('object')
+    length = platform.length
+    platlist = []
+    for i in range(length):
+        x = int(platform[i].attributes['x'].value)
+        y = int(platform[i].attributes['y'].value)
+        w = int(platform[i].attributes['width'].value)
+        h = int(platform[i].attributes['height'].value)
+        platlist.append(plat(pygame.Rect(x,y,w,h)))
+
+    return platlist
