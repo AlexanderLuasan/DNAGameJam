71c4f3e6d48b4c78ed41db2cc6405c4337cf3790
Chia-Tse KUO <chiatskuo2-c@my.cityu.edu.hk>
2019-02-22 13:14:20 +0800

    keys partially broken, right key works

--- DNAGameJam/player.py
+++ DNAGameJam/player.py
@@ -1,5 +1,5 @@
 from CollisionObj import CollisionObj
-
+maxspeed = 3
 
 class player(CollisionObj):
     def __init__(self,CollisionRect):
@@ -14,18 +14,36 @@ class player(CollisionObj):
             self.velx+=1
         if(self.keys[1]==True):
             self.velx-=1
-        if(self.velx>10):
-            velx=10
-        if(self.velx<-10):
-            velx=-10
+        if(self.velx>maxspeed):
+            self.velx-=1;
+        if(self.velx<-maxspeed):
+            velx=-maxspeed
         if(self.keys[2]==True):
             self.vely+=1
         if(self.keys[3]==True):
             self.vely-=1
-        if(self.vely>10):
-            vely=10
-        if(self.vely<-10):
+        if(self.vely>maxspeed):
+            vely=maxspeed
+        if(self.vely<-maxspeed):
             vely=-10
+        
+        if(self.keys[0]==False and self.keys[1]==False):
+            self.velx-=0.1
+            if(self.velx<0):
+                self.velx=0;
+        if(self.keys[1]==False and self.keys[0]==False):
+            self.velx+=0.1
+            if(self.velx>0):
+                self.velx=0;
+        if(self.keys[2]==False and self.keys[3]==False):
+            self.vely-=0.1
+            if(self.vely<0):
+                self.vely=0;
+        if(self.keys[3]==False and self.keys[2]==False):
+            self.vely+=0.1
+            if(self.vely>0):
+                self.vely=0;
+        
         #move collision box
         self.collisionRect.x=self.collisionRect.x+self.velx
         self.collisionRect.y=self.collisionRect.y+self.vely
